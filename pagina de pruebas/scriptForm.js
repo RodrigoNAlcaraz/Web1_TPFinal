584b166861120189078a858b2c7ad493bbd2fb80
RodrigoAlcaraz <rodrigo95alcaraz@gmail.com>
2023-05-31 18:43:17 -0300

    script

--- pagina de pruebas/scriptForm.js
+++ pagina de pruebas/scriptForm.js
@@ -17,12 +17,16 @@ document.addEventListener('DOMContentLoaded', function () {
     let apellidoError = document.getElementById('apellidoError');
     let emailError = document.getElementById('emailError');
     let celularError = document.getElementById('celularError');
+    let validado = true;
+    let mensajes = document.getElementById('mensajes');
 
     // validar nombre 
     if (nombre.trim() === '') {
       nombreError.innerHTML = '*El campo NOMBRE debe estar completo*';
+      validado = false;
     } else if (nombre.trim().length < 3) {
-      nombreError.innerHTML = '*debe contener 3 caracteres como minimo*';
+      nombreError.innerHTML = '*debe contener 3 caracteres como mínimo*';
+      validado = false;
     } else {
       nombreError.textContent = '';
     }
@@ -30,16 +34,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // validar apellido
     if (apellido.trim() === '') {
       apellidoError.innerHTML = '*El campo APELLIDO debe estar completo*';
+      validado = false;
     } else if (apellido.trim().length < 3) {
-      apellidoError.innerHTML = '*debe contener 3 caracteres como minimo*';
+      apellidoError.innerHTML = '*debe contener 3 caracteres como mínimo*';
+      validado = false;
     } else {
       apellidoError.textContent = '';
     }
 
     // validar celular
-
     if (celular.length < 10 || celular.length > 13) {
-      celularError.innerHTML = '*minimo 10 y máximo 13 números*';
+      celularError.innerHTML = '*mínimo 10 y máximo 13 números*';
+      validado = false;
     } else {
       celularError.textContent = '';
     }
@@ -47,14 +53,30 @@ document.addEventListener('DOMContentLoaded', function () {
     // validar correo
     if (email.trim() === '') {
       emailError.innerHTML = '*El campo CORREO debe estar completo*';
+      validado = false;
     } else if (!reg_correo.test(email)) {
       emailError.innerHTML = '*error debe ser ejemplo: algo@algo.com*';
+      validado = false;
     } else {
       emailError.textContent = '';
     }
 
+    mensajes.innerHTML = '';//evita que se junten los mensajes uno abajo del otro
 
-
+    //mostrar el mensaje abajo, si es falso que hay error sino mostrar los datos
+    if (!validado) {
+      let p = document.createElement("p");
+      p.innerHTML = "Hay errores en el formulario";
+      p.style.textAlign="center";
+      p.style.color="red";
+      mensajes.appendChild(p);
+    } else {
+      let p = document.createElement("p");
+      p.innerHTML = "Nombre completo: " + apellido + " " + nombre + ", Celular: " + celular + ", correo: " + email;
+      mensajes.appendChild(p);
+      form.reset();
+    }
   });
 });
+
 
